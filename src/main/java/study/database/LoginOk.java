b0676040692984b71930307259213302e9e3948e
herb39 <herb_39@nate.com>
2023-04-27 01:22:49 +0900

    20230427_0122

--- src/main/java/study/database/LoginOk.java
+++ src/main/java/study/database/LoginOk.java
@@ -2,6 +2,8 @@ package study.database;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -24,22 +26,46 @@ public class LoginOk extends HttpServlet{
 		
 		PrintWriter out = response.getWriter();
 		
+		Date today = new Date();
+		SimpleDateFormat sdfToday = new SimpleDateFormat("yyyy-MM-dd");
+		String strNowDate = sdfToday.format(today);
+		
+		int todayCount = vo.getTodayCount();
+		int point = 0;
+		String lastDate = vo.getLastDate();
+		
 		if (vo.getName() != null) {
 			// 회원 인증 성공
 			// 방문 포인트 / 최종 접속일 / 방문 카운트 처리
 			// db 최종접속일(10자리)와 시스템날짜(10자리) 비교
 			// 같으면 todayCount = vo.getTodayCount()+1 / 다르면 todayCount = 0
-			// dao.setPointPlus(mid, todayCount);
-			dao.setPointPlus(mid);
+			// dao.setPointPlus(mid, todayCount);			
+
+			if (lastDate.substring(0, 10).equals(strNowDate)) {
+				todayCount++;
+				System.out.println("재방문 카운트+");
+				if (todayCount <= 5) {
+					point = vo.getPoint() + 10;
+					System.out.println("재방문 카운트+ 포인트+");
+				}
+			} else {
+				todayCount = 1;
+				point = vo.getPoint() + 10;
+				System.out.println("첫방문");
+			}
 			
 			// 1. 자주 사용하는 자료 세션에 저장(아이디, 성명, 닉네임)
 			HttpSession session = request.getSession();
 			session.setAttribute("sMid", mid);
 			session.setAttribute("sName", vo.getName());
-			session.setAttribute("sPoint", vo.getPoint()+10);
+			session.setAttribute("sPoint", vo.getPoint());
+			session.setAttribute("sTodayCount", vo.getTodayCount());
 			session.setAttribute("sLastDate", vo.getLastDate());
-			session.setAttribute("sTodayCount", vo.getTodayCount()+1);
-			
+
+			dao.setPointPlus(mid, point, todayCount);
+
+			vo = dao.getLoginCheck(mid, pwd);
+
 			out.print("<script>");
 			out.print("alert('"+mid+"님 로그인 되었습니다.');");
 			out.print("location.href='"+request.getContextPath()+"/study/0428_database/memberMain.jsp';");
--- src/main/webapp/study/0428_database/login.jsp
+++ src/main/webapp/study/0428_database/login.jsp
@@ -5,7 +5,7 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title>title</title>
+	<title>login.jsp</title>
 	<jsp:include page="/include/bs4.jsp" />
 </head>
 <body>
